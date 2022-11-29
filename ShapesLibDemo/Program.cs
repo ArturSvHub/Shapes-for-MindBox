55517515248678eab798fa5d1d56b4bd4cd915e7
Artur Sveshnikov <73856281+ArturSvHub@users.noreply.github.com>
2022-11-30 00:00:26 +0300

    Without ex test running.

--- ShapesLibDemo/Program.cs
+++ ShapesLibDemo/Program.cs
@@ -9,7 +9,6 @@ namespace ShapesLibDemo
             var s = new Circle(10);
             var tr = new Triangle(5, 4, 2);
             var rect = new Rectangle(10,20);
-            var tz = new Triangle(10,20,-1);
             Console.WriteLine(s);
             Console.WriteLine("\n");
             Console.WriteLine(tr);
--- ShapesLibDemo/Rectangle.cs
+++ ShapesLibDemo/Rectangle.cs
@@ -1,12 +1,5 @@
 ﻿using Shapes.BaseImplement;
-using Shapes.Implements;
 using Shapes.Interfaces;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ShapesLibDemo
 {
